9dcbd3b25e2499ce593c05ce126bcebc06056292
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-17 16:45:26 +0200

    Finished Ex02

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,15 @@
+/*The function prints digits, on a single line, in an ascending order */
+
+int		ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	char n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		ft_putchar(n);
+		n++;
+	}
+}
