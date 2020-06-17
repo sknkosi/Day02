b40e5acedc29a7567a9014fec00654f916c291eb
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-17 16:30:44 +0200

    Finished Ex01

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,15 @@
+/*The function prints alphabets in lowercase, on a single line, on a descending order, starting from the letter ’z’to 'a'. */
+
+int		ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		ft_putchar(z);
+		z--;
+	}
+}
