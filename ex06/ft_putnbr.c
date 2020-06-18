1ae112ccfec9b382f35200fb59a8312807a7b964
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-18 10:19:02 +0200

    Finished Ex06

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -1,12 +1,21 @@
 /*The function prints a number entered as a parameter. The functionhas is able to display all possible values within an int type variable.*/ 
 
-int ft_putchar (char *int)
+int ft_putchar(char c);
 
-void ft_putnbr(int nb)
+void ft_putnbr(int nb) 
 {
-	int n;
-	n = 0;
-	while (int[n])
-		ft_putchar(int[n++]);w
-
-}	
+   	char c; 
+	
+	if (nb < 0) 
+	{ 
+		nb = -nb; 
+		ft_putchar('-');
+   	} 
+	if (nb < 10) 
+		ft_putchar(nb + '0'); 
+	else 
+	{ 
+		ft_putnbr(nb / 10); 
+		ft_putnbr(nb % 10);
+   	} 
+}
