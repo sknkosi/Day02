e4a5a1673d50f02e2b516f4925ad363b33d8df34
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 15:26:46 +0200

    Finished Ex06

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 15:25:43 by sknkosi           #+#    #+#             */
+/*   Updated: 2020/06/23 15:26:02 by sknkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 /*The function prints a number entered as a parameter. The functionhas is able tto display all possible values within an int type variable.*/ 
