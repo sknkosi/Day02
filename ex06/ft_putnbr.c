9e4dffbc13ac540a8c0d96206c6c2c677c3d356b
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 13:44:17 +0200

    Finished Ex06

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -1,21 +1,33 @@
-/*The function prints a number entered as a parameter. The functionhas is able to display all possible values within an int type variable.*/ 
+#include <unistd.h>
 
-int ft_putchar(char c);
+/*The function prints a number entered as a parameter. The functionhas is able tto display all possible values within an int type variable.*/ 
 
-void ft_putnbr(int nb) 
+
+void ft_putchar(char c)
 {
-   	char c; 
-	
-	if (nb < 0) 
-	{ 
-		nb = -nb; 
+	write(1,&c,1);
+}
+
+void ft_putnbr(int nb)
+{
+	char c;
+
+	if (nb < 0)
+	{
+		nb = -nb;
 		ft_putchar('-');
-   	} 
-	if (nb < 10) 
-		ft_putchar(nb + '0'); 
-	else 
-	{ 
-		ft_putnbr(nb / 10); 
+	}
+	if (nb < 10)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
-   	} 
+	}
+}
+
+int main()
+{
+	ft_putnbr();
+	return 0;
 }
