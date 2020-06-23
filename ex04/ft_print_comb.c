ef2a8994f22c791f60b419e75381cd44c07f4363
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 10:59:55 +0200

    Finished Ex04

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -1,40 +1,42 @@
+#include <unistd.h>
+
 /*The function displays different combinations of three different digits in an  ascending order.*/
 
-int	ft_putchar(char c);
-void xyz_putchar(char x, char y, char z);
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
 
 void	ft_print_comb(void)
 {
-	char d;
-	char b;
-	char a;
+	char	n1;
+	char	n2;
+	char	n3;
 
-	d = '0';
-	while (d <= '7')
+	n1 = '0' - 1;
+	while (++n1 <= '9')
 	{
-		b = d + 1;
-		while (b <= '8')
+		n2 = n1;
+		while (++n2 <= '9')
 		{
-			a = b + 1;
-			while (a <= '9')
+			n3 = n2;
+			while (++n3 <= '9')
 			{
-				xyz_putchar(d, b, a);
-				if (d != '7' || b != '8' || a != '9')
-				{
+				if (n1 != '0' || n2 != '1' || n3 != '2')
 					ft_putchar(',');
+				if (n1 != '0' || n2 != '1' || n3 != '2')
 					ft_putchar(' ');
-				}
-				a++;
+				ft_putchar(n1);
+				ft_putchar(n2);
+				ft_putchar(n3);
 			}
-			b++;
 		}
-		d++;
 	}
+	ft_putchar('\n');
 }
 
-void	xyz_putchar(char x, char y, char z)
+int main()
 {
-	ft_putchar(x);
-	ft_putchar(y);
-	ft_putchar(z);
+ 	ft_print_comb();
+	return 0;
 }
