e28b66f595053b93f5e24fb9fe2fbabf17d29053
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 12:40:17 +0200

    Finished Ex05

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -14,6 +14,7 @@ void ft_print_comb2(void)
 
 	a = 0;
 	b = 0;
+
 	while (a < 100)
 	{
 		b = a + 1;
@@ -24,6 +25,7 @@ void ft_print_comb2(void)
 			ft_putchar(' ');
 			ft_putchar(b / 10 + '0');
 			ft_putchar(b % 10 + '0');
+		
 			if ((a / 10 != 9) || (a % 10 != 8))
 			{
 				ft_putchar(',');
@@ -34,5 +36,4 @@ void ft_print_comb2(void)
 		a++;
 	}
 	ft_putchar('\n');
-
 }
