b0cf9cebcd4762e67780a5cdf3e4c2e74af8b458
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 16:04:27 +0200

    Finished Ex06

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -1,39 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 15:25:43 by sknkosi           #+#    #+#             */
-/*   Updated: 2020/06/23 15:26:02 by sknkosi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
 /*The function prints a number entered as a parameter. The functionhas is able tto display all possible values within an int type variable.*/ 
 
-
-void ft_putchar(char c)
-{
-	write(1,&c,1);
-}
+void ft_putchar(char c);
 
 void ft_putnbr(int nb)
 {
-	char c;
-
-	if (nb < 0)
+	if (nb < 10 && nb >= 0)
 	{
-		nb = -nb;
-		ft_putchar('-');
-	}
-	if (nb < 10)
 		ft_putchar(nb + '0');
-	else
+	}
+	else if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putchar(nb % 10 + '0');
+		if (nb < 0)
+			ft_putchar('-');
+	}
+	else
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
 	}
 }
