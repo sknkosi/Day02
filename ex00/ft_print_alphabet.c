18cff10a6fb13a1afcb9c3889d0a7bf549b715b9
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 08:30:45 +0200

    Finished Ex00

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -2,12 +2,10 @@
 
 /* The function prints the alphabet in lowercase, on a single line, in an ascending order, starting from the letter ’a’ to 'z'*/
 
-
-int ft_putchar(char c)
-/*{
-//	write(1, &c,1);
-//	return (0);
-}*/
+void ft_putchar(char c)
+{
+	write(1, &c,1);
+}
 void ft_print_alphabet(void)
 {
 	char a;
@@ -19,9 +17,3 @@ void ft_print_alphabet(void)
 		a++;
 	}
 }
-
-/*int main()
-{
-	ft_print_alphabet();
-	return 0;
-}*/
