c5241c8a265e6831d8e17711a3971c606d4a6e9e
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-23 09:59:09 +0200

    Finished Ex03

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,15 +1,31 @@
+#include <unistd.h>
+
 /*The function displays ’N’ or ’P’ depending on the integer’s sign entered as a parameter. If n is negative, it's prints ’N’ and if n is positive or null, it prints ’P’ */
 
-int ft_putchar(char c);
-
-void ft_is_negative(int n)
+void ft_putchar(int n)
 {
+	write(1,&n,1);
+}
+
+void ft_is_negative()
+{
+	int n;
+	n =0;
+	while (n>=-1 && n<=1)
+	{
 	if (n < 0)
-	{
-		ft_putchar('N');
-	}
-	else
-	{
+	ft_putchar('N');
+
+	else if (n == 0 || n =='\0' || n > 0)
+	
 		ft_putchar('P');
+	break;
 	}
+
+}
+
+int main()
+{
+	ft_is_negative();
+	return 0;
 }
